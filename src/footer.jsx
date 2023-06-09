7e91519f6b1401d4fcac3be5493a36647a13ebeb
ASLaskin <andrewlaskin@ufl.edu>
2023-06-09 17:20:34 -0400

    small change to css and fixed git link

--- src/footer.jsx
+++ src/footer.jsx
@@ -9,7 +9,7 @@ export function Footer() {
         <>
             <section className="footer">
                 <div className="footer-image">
-                    <a href="https://github.com/ASLaskin" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"alt="github" /></a>
+                    <a href="https://github.com/ASLaskin/BubbleSort-Visualizer" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"alt="github" /></a>
                     <a href='https://www.linkedin.com/in/andrew-laskin-44015424a/' target="_blank"><img src='https://w7.pngwing.com/pngs/276/472/png-transparent-linkedin-computer-icons-blog-logo-watercolor-butterfly-angle-text-rectangle-thumbnail.png'
                         alt='linkedin' /></a>
                 </div>
